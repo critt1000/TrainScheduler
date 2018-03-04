81c40b953fd33d5b5f8900092dd4867b0917bc65
Christian Rittmann <crittmann@hotmail.comgit config --global user.name Christian>
2018-03-03 20:44:39 -0600

    2nd 3/3/2018 commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,80 +1,83 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAKI44IKQR4Bm5y5GmFJIRj9fxzmH7ZXzM",
-    authDomain: "trainscheduler-526f0.firebaseapp.com",
-    databaseURL: "https://trainscheduler-526f0.firebaseio.com",
-    projectId: "trainscheduler-526f0",
-    storageBucket: "",
-    messagingSenderId: "18012282612"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAKI44IKQR4Bm5y5GmFJIRj9fxzmH7ZXzM",
+  authDomain: "trainscheduler-526f0.firebaseapp.com",
+  databaseURL: "https://trainscheduler-526f0.firebaseio.com",
+  projectId: "trainscheduler-526f0",
+  storageBucket: "trainscheduler-526f0.appspot.com",
+  messagingSenderId: "18012282612"
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  $('#submitBtn').on('click', function () {
-    event.preventDefault();
+// Creating Initial Values
+var trainName = "";
+var destination = "";
+var frequency = 0;
+var firstArrival = "";
+var nextArrival = "";
+var minutesAway = 0;
+
+// Button Click function
+$('#submitBtn').on('click', function (event) {
+  event.preventDefault();
+
+  //Grab user input and store in variables
+  trainName = $('#name').val().trim();
+  destination = $('#destination').val().trim();
+  frequency = $('#frequency').val().trim();
+  firstArrival = $('#firstTime').val().trim();
+
+  // Calculate nextArrival and minutesAway
+  /*
+  function calcData() {
+  var difTime = moment().dif(moment(firstArrival), "minutes");
+  var modTime = difTime % frequency;
+  minutesAway = frequency - modTime;
+  nextArrival = moment().add(minutesAway, "minutes");
   
-    var trainName;
-    var destination;
-    var frequency;
-    var nextArrival;
-    var minutesAway;
-    var firstArrival
-  
-  
-  
-    trainName = $('#name').val().trim();
-    destination = $('#destination').val().trim();
-    firstArrival = $('#firstTime').val().trim();
-    frequency = $('#frequency').val().trim();
-  
-    // calc months worked
-    var dateFormat = "YYYY-MM-DD";
-    var convertedStartDate = moment(startDate, dateFormat);
-    monthsWorked = moment().diff(convertedStartDate, "months");
-  
-    // calc total billed
-    var totalBilled = monthsWorked * rate;
-  
-    // DONE: add to database here -- use .push() instead of .set(), include dateAdded: firebase.database.ServerValue.TIMESTAMP
-    database.ref().push({
-      name: name,
-      role: role,
-      startDate: startDate,
-      monthsWorked: monthsWorked,
-      rate: rate,
-      totalBilled: totalBilled,
-      dateAdded: firebase.database.ServerValue.TIMESTAMP
-    });
-  
+  }
+  */
+
+  // Push Data to Database
+  database.ref().push({
+    trainName: trainName,
+    destination: destination,
+    frequency: frequency,
+    firstArrival: firstArrival,
+    
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
   });
-  
-  // add firebase watcher
-  database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
-  
-    var sv = snapshot.val();
-  
-    // DONE: log the data from the database
-    console.log(sv.name);
-    console.log(sv.role);
-    console.log(sv.startDate);
-    console.log(sv.monthsWorked);
-    console.log(sv.rate);
-    console.log(sv.totalBilled);
-  
-    // DONE: push the new stuff to the HTML
-    var html =
+
+});
+
+// add firebase watcher
+database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function (snapshot) {
+
+  var sv = snapshot.val();
+
+  // DONE: log the data from the database
+  console.log(sv.trainName);
+  console.log(sv.destination);
+  console.log(sv.frequency);
+  console.log(sv.nextArrival);
+  console.log(sv.minutesAway);
+  console.log(sv.firstArrival);
+
+  // DONE: push the new stuff to the HTML
+  var html =
     '<tr class="something">' +
-      '<td>' + sv.name + '</td>' +
-      '<td>' + sv.role + '</td>' +
-      '<td>' + sv.startDate + '</td>' +
-      '<td>' + sv.monthsWorked + '</td>' +
-      '<td>$ ' + sv.rate + '</td>' +
-      '<td>$ ' + sv.totalBilled + '</td>' +
+    '<td>' + sv.trainName + '</td>' +
+    '<td>' + sv.destination + '</td>' +
+    '<td>' + sv.frequency + '</td>' +
+    '<td>' + sv.nextArrival + '</td>' +
+    '<td>$ ' + sv.minutesAway + '</td>' +
+    '<td>$ ' + sv.firstArrival + '</td>' +
     '</tr>';
-  
-    $('#outPutRow').append(html);
-  
-  }, function (errorObject) {
-    console.log("The Read Failed: " + errorObject.code);
-  });
+
+  $('#outPutRow').append(html);
+
+}, function (errorObject) {
+  console.log("The Read Failed: " + errorObject.code);
+});
